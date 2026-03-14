85b241c94700400697a89d09425b4a4133acf279
Alexander Dmitrenko <alex@seapay.me>
2026-03-14 23:00:03 +0100

    Initial Commit

---
+++ SeaPay KYC/SeaPay_KYCApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeaPay_KYCApp.swift
+//  SeaPay KYC
+//
+//  Created by Alexander Dmitrenko on 14/3/26.
+//
+
+import SwiftUI
+
+@main
+struct SeaPay_KYCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
